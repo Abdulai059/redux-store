87e4bd9dfd20d2e47461d83fa85826d6f565c882
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-13 16:20:51 +0000

    first commit

--- src/Features/accounts/AccountOperations.jsx
+++ src/Features/accounts/AccountOperations.jsx
@@ -14,16 +14,25 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
 
   console.log(balance);
 
   function handleDeposit() {
-    if (!depositAmount) return;
+    if (!depositAmount) {
+      alert("Enter an amount");
+      return;
+    }
 
-    dispatch(deposit(depositAmount));
+    if (!currency) {
+      alert("Select a currency");
+      return;
+    }
+
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
@@ -65,7 +74,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
