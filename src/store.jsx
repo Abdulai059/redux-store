90d9bd56a3e12951dca59281b60d68bdf5492a8b
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-13 17:16:56 +0000

    first commit

--- src/store.jsx
+++ src/store.jsx
@@ -1,19 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 
-import { thunk } from "redux-thunk";
 import accountReducer from "./Features/accounts/accountSlice";
 import customerReducer from "./Features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
-
 export default store;
- 
